70ae4cc80cc11c2d35665f9a0a8b8a122e34f036
Rishabh Saini <burishabhsaini23@gmail.com>
2026-03-19 17:44:20 +0530

    Add math extraction

--- features/main.py
+++ features/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from image_processor import ImageProcessor
 from ocrengine import OCREngine
+from formula_engine import FormulaEngine
 from handwriting_engine import HandwritingEngine
 import io
 from PIL import Image
@@ -10,11 +11,13 @@ from PIL import Image
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-# Initialize your modules
+# Init modules
 processor = ImageProcessor()
 ocr = OCREngine()
+formula = FormulaEngine()
 handwriting = HandwritingEngine()
 
+#Text extraction endpoint
 @app.post("/api/v1/extract")
 async def extract_content(file: UploadFile = File(...)):
     try:
@@ -44,7 +47,28 @@ async def extract_content(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
+#Formual Endpoint
+@app.post("/api/v1/extract/math")
+async def extract_math_content(file: UploadFile = File(...)):
+    try:
+        file_bytes = await file.read()
+        processor.validate_image(file_bytes, file.filename)
+        
+        # We don't grayscale for math, the pix2tex model prefers the raw image
+        import io
+        from PIL import Image
+        raw_image = Image.open(io.BytesIO(file_bytes))
+        
+        extracted_latex = formula.extract_latex(raw_image)
+        
+        return {
+            "status": "success",
+            "data": { "type": "latex", "content": extracted_latex }
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
+#Handwriting endpoint
 @app.post("/api/v1/extract/handwriting")
 async def extract_handwriting_content(file: UploadFile = File(...)):
     try:
@@ -54,7 +78,7 @@ async def extract_handwriting_content(file: UploadFile = File(...)):
         # Open the raw image
         raw_image = Image.open(io.BytesIO(file_bytes))
         
-        # Extract using the new TrOCR model
+        # Extract using the  TrOCR model
         extracted_text = handwriting.extract_handwriting(raw_image)
         
         return {
@@ -64,7 +88,6 @@ async def extract_handwriting_content(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
